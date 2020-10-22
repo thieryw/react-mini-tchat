ca97964ae092c0f875556ad1ce2d669a3873c417
William Thiery <joseph.garrone.gj@gmail.com>
2020-10-22 20:12:58 +0200

    update

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+export const App:React.FunctionComponent = ()=>{
+
+
+    return(
+        <div>
+            <h1>Helloo World !!</h1>
+        </div>
+    )
+}
---
+++ src/logic.ts
@@ -0,0 +1,67 @@
+import {Evt} from "evt";
+
+
+
+
+type Message = {
+    description: string;
+    direction: "incoming" | "outgoing";
+}
+
+type User = {
+    name: String;
+    messages: Message[];
+}
+export type Store = {
+    users: User[];
+    sendMessage: (params: {emitter: User; receiver: User; description: string}) => Promise<void>;
+
+    evtMessageSent: Evt<void>;
+
+}
+
+export async function getStore(): Promise<Store>{
+
+    const simulateNetworkDelay = (delay: number)=>{
+        return new Promise<void>(resolve => setTimeout(resolve, delay));
+    }
+
+    const users: User[] = [
+        {
+            "messages": [],
+            "name": "User 1"
+        },
+        {
+            "name": "User 2",
+            "messages": [],
+        }
+    ]
+
+    const store: Store = {
+        "evtMessageSent": new Evt(),
+
+        "sendMessage": async params => {
+            await simulateNetworkDelay(300);
+
+            const {emitter, receiver, description} = params;
+
+            emitter.messages.push({description, "direction": "outgoing"});
+            receiver.messages.push({description, "direction": "incoming"})
+
+            store.evtMessageSent.post();
+
+        },
+
+        users
+    }
+
+    await simulateNetworkDelay(1500);
+
+    return store;
+
+
+
+}
+
+
+
