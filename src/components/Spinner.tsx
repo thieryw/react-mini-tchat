24fbb31647bf8978dcdc6a6656638a8df52447b8
William Thiery <williamalexthiery@hotmail.fr>
2020-10-26 22:33:46 +0100

    update

--- src/components/Spinner.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const Spinner: React.FunctionComponent = ()=> <i className="fa fa-spin fa-spinner" />;
--- src/logic.ts
+++ src/logic.ts
@@ -1,114 +1,36 @@
 import {Evt, NonPostableEvt, ToPostableEvt} from "evt";
 
 
-
-
-
 type Message = {
     description: string;
     direction: "incoming" | "outgoing";
-    receiver: User;
-    emitter: User;
-
-}
-
-type conversation = {
-    messages: Message[];
-    participants: User[];
-}
-
-
-
-type User = {
-    name: String;
-    messages: Message[];
-    interlocutor: User | undefined;
     id: number;
 }
+
+type Conversation = {
+    interlocutor: User;
+    messages: Message[];
+    id: number;
+}
+type User = {
+    name: string;
+    contacts: User[];
+    conversations: Conversation[];
+    id: number;
+
+}
+
 export type Store = {
     users: User[];
-    sendMessage: (params: {user: User; description: string}) => Promise<void>;
-    changeInterlocutor: (params: {user: User; newInterlocutor: User}) => Promise<void>;
+    selectContact: (params: {user: User; contact: User}) => Promise<void>;
+    sendMessage: (params: {user: User; contact: User}) => Promise<void>;
 
+    evtContactSelected: NonPostableEvt<Parameters<Store["selectContact"]>[0]>;
     evtMessageSent: NonPostableEvt<Parameters<Store["sendMessage"]>[0]>;
-    evtInterlocutorChanged: NonPostableEvt<Parameters<Store["changeInterlocutor"]>[0]>;
-
 }
 
-export async function getStore(): Promise<Store>{
-
-    const simulateNetworkDelay = (delay: number)=>{
-        return new Promise<void>(resolve => setTimeout(resolve, delay));
-    };
 
 
-    const users: User[] = (()=>{
-        const out:User[] = [];
-
-        for(let i = 0; i < 3; i++){
-            out.push({
-                "id": i,
-                "messages": [],
-                "name": `user ${i+1}`,
-                "interlocutor": undefined
-            })
-        }
-
-        return out;
-
-    })(); 
-
-    const store: ToPostableEvt<Store> = {
-        "evtMessageSent": new Evt(),
-        "evtInterlocutorChanged": new Evt(),
-
-        "sendMessage": async params => {
-            await simulateNetworkDelay(300);
-
-            const {user, description} = params;
-            if(user.interlocutor === undefined){
-                return;
-            }
-
-            user.messages.push({
-                description, 
-                "direction": "outgoing", 
-                "receiver": user.interlocutor,
-                "emitter": user
-            });
-            user.interlocutor.messages.push({
-                description, 
-                "direction": "incoming", 
-                "receiver": user.interlocutor,
-                "emitter": user
-            });
-
-
-            store.evtMessageSent.post(params);
-
-        },
-
-        "changeInterlocutor": async params => {
-            const {user, newInterlocutor} = params;
-
-            await simulateNetworkDelay(300);
-
-            user.interlocutor = newInterlocutor;
-
-            store.evtInterlocutorChanged.post(params);
-
-        },
-
-        users
-    }
-
-    await simulateNetworkDelay(1500);
-
-    return store;
-
-
-
-}
 
 
 
